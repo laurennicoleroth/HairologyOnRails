dbf731d8d91600d7845760d0cef1c00caf4e1a53
Lauren N. Roth <laurenroth@gmail.com>
2015-04-06 17:04:10 -0400

    Add email validation on user model. 	deleted:    app/assets/stylesheets/users.scss 	modified:   app/models/user.rb 	modified:   test/models/user_test.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :name,  presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
   validates :password, presence: true, length: { maximum: 50 }
 end
