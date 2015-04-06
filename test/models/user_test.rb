3f84db1afe10b9eb8c03f19c3036aa4ed1d4fdb4
Lauren N. Roth <laurenroth@gmail.com>
2015-04-06 17:14:47 -0400

    Validate email address without case sensitivity. 	modified:   app/models/user.rb 	modified:   test/models/user_test.rb

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com", password: "superduper123")
+    @user = User.new(name: "Example User", email: "user@example.com",
+      password: "superduper123", password_confirmation: "superduper123")
   end
 
   test "should be valid" do
